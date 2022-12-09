95cf3348ede622a4018fae1d0552a63878f12856
eyosolutions <emmanuel.y.odoom@gmail>
2022-12-09 08:40:47 -0500

    Final changes

--- final_project/server.py
+++ final_project/server.py
@@ -22,5 +22,6 @@ def frenchToEnglish():
 def renderIndexPage():
     # Write the code to render template
     return render_template('index.html')
+    
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
